0c3405d23f2d4a6c17b8ea76ef791d942023de76
배포서버 <tttkhs@naver.com>
2018-11-02 14:46:13 +0000

    server handshaking

--- Server_Node-js/passport.js
+++ Server_Node-js/passport.js
@@ -9,7 +9,7 @@ module.exports = function (app) {
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '',
+    password: '880918',
     database: 'toeic_solver'
   };
   var sessionStore = new MySQLStore(options);
@@ -69,29 +69,5 @@ module.exports = function (app) {
         return done(null, user[0]);
       })
     }));
-<<<<<<< HEAD
-    passport.use(new KakaoStrategy(
-      KakaoStrategy_options,
-    function(request,accessToken, refreshToken, profile, done){
-      // 사용자의 정보는 profile에 들어있다.
-        db.query('SELECT id FROM auth_kakao WHERE id = ?', profile.id, function (err, result) {
-          if (result[0]) {
-            console.log("login with kakao", profile,result);
-            return done(null, profile);
-          } // 회원 정보가 있으면 로그인
-          db.query('INSERT INTO auth_kakao (id,name,nickname,profile_image) VALUES (?,?,?,?)',
-          [profile.id,profile.username,profile.displayName,profile._json.properties.profile_image], function (err, result) {
-            console.log("register with kakao",profile , result);
-            request.login(profile, function (err) {
-              request.session.save(function () {
-                return done(null, profile); // 새로운 회원 생성 후 로그인
-              })
-            })
-          });
-        });
-    }
-  ))
-=======
->>>>>>> 6d9f1465d49147864329954a4427daf90783a5c2
   return passport;
 }
